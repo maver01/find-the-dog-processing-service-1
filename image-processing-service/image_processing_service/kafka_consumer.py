f23efbfc236989fd13bd995b5f9495650cce2b3f
maver01 <matteo.sabadin4@gmail.com>
2024-09-11 15:48:18 +0200

    added black and white microservice

--- image-processing-service/image_processing_service/kafka_consumer.py
+++ image-processing-service/image_processing_service/kafka_consumer.py
@@ -1,29 +1,63 @@
-# kafka_consumer.py
-"""
-This script is responsible for consuming images from a Kafka topic, processing them using the image_processor module, and producing the processed images to another Kafka topic.
-
-Attributes:
-    consumer (KafkaConsumer): The Kafka consumer instance used to consume images from the 'image-input-topic'.
-    producer (KafkaProducer): The Kafka producer instance used to produce processed images to the 'image-output-topic'.
-"""
-
-
+import logging
+import base64
+import io
+from PIL import Image
 from kafka import KafkaConsumer, KafkaProducer
-from image_processing_service import image_processor
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Kafka Consumer Configuration
 consumer = KafkaConsumer(
-    'image-input-topic',
+    'image-processing-topic',
     bootstrap_servers=['localhost:9092'],
     auto_offset_reset='earliest',
     enable_auto_commit=True,
-    value_deserializer=lambda x: x
-    )
+    value_deserializer=lambda x: x, # Receive raw bytes
+)
 
+# Kafka Producer Configuration
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9092'],
-    value_serializer=lambda x: x)
+    value_serializer=lambda x: x,  # Send raw bytes
+)
 
+def process_image(image_bytes):
+    '''
+    Process the image bytes using the image processor. Turn image in black and white, using pillow.
+    Input: 
+        image_bytes: bytes
+    Output:
+        processed_image: bytes
+    '''
+    # Decode the image bytes
+    decoded_bytes = base64.b64decode(image_bytes)
+    # Turn the image into black and white using pillow
+    image = Image.open(io.BytesIO(decoded_bytes))
+    processed_image = image.convert('L')
+    # Save the processed image to raw bytes
+    buffered = io.BytesIO()
+    processed_image.save(buffered, format="PNG")
+    processed_image_bytes = buffered.getvalue()
+    # Encode the processed image bytes
+    processed_image_string = base64.b64encode(processed_image_bytes)
+    return processed_image_string
+
+
+
+logging.info('Started Kafka consumer')
 for message in consumer:
-    image_bytes = message.value
-    processed_image_bytes = image_processor.process_image(image_bytes)
-    producer.send('image-output-topic', value=processed_image_bytes)
+    try:
+        received_string = message.value  # This is received string
+        logging.info('Received image from Kafka topic')
+        
+        # Process the image bytes using the image processor
+        processed_image = process_image(received_string)
+        logging.info('Processed image')
+        
+        # Send the processed image back to Kafka
+        producer.send('image-output-topic', value=processed_image)
+        logging.info('Sent processed image to Kafka topic')
+        
+    except Exception as e:
+        logging.error(f"Error processing message: {e}")
